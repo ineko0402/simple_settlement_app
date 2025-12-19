23da83e0484d313236a233cdad9f4a55fbce6e9a
ineko0402 <a.catnapkoala@gmail.com>
2025-12-19 21:35:31 +0900

    モバイルアプリライク

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js
+const CACHE_NAME = 'accounting-app-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap',
+    'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
