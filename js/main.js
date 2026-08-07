b87711eacc05551fea04d729866757e21286e7b9
ineko0402 <52097123+ineko0402@users.noreply.github.com>
2026-08-07 11:36:58 +0900

    Fix pocket ledger file content: js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/root/.profile: line 9: /dev/null: Permission denied
 import { initTheme } from './theme.js';
 import { initNavigation } from './navigation.js';
 import { initSwipe } from './swipe.js';
